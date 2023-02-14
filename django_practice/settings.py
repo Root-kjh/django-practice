dc8b0dbf362a161a939ca1ec3ac56b486f2e7be1
root-kjh <wldnro3141@gmail.com>
2023-02-14 23:25:53 +0900

    feat: add rest_framework app to INSTALLED_APPS

--- django_practice/settings.py
+++ django_practice/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "studies",
+    'rest_framework',
+    'studies',
 ]
 
 MIDDLEWARE = [
