df23fce651572d8ee546cfba68d596d17d1e45a3
root-kjh <wldnro3141@gmail.com>
2023-02-23 22:57:57 +0900

    fix: change study serializer

--- studies/serializers.py
+++ studies/serializers.py
@@ -1,29 +1,30 @@
 from rest_framework.serializers import ModelSerializer
+from drf_writable_nested.serializers import WritableNestedModelSerializer
 
 from .models import Study, Intervention, Condition, Eligibility
 
 class InterventionSerializer(ModelSerializer):
     class Meta:
         model = Intervention
-        fields = ['intervention_type', 'name', 'description']
+        fields = ['intervention_type', 'name', 'description', 'original_intervention', 'locale']
 
 
 class ConditionSerializer(ModelSerializer):
     class Meta:
         model = Condition
-        fields = ['name']
+        fields = ['name', 'original_condition', 'locale']
 
 
 class EligibilitySerializer(ModelSerializer):
     class Meta:
         model = Eligibility
-        fields = ['gender', 'minimum_age', 'maximum_age', 'healthy_volunteers', 'criteria']
+        fields = ['gender', 'minimum_age', 'maximum_age', 'healthy_volunteers', 'criteria', 'original_eligibility', 'locale']
 
 
-class StudySerializer(ModelSerializer):
-    interventions = InterventionSerializer(many=True, read_only=True)
-    conditions = ConditionSerializer(many=True, read_only=True)
-    eligibilities = EligibilitySerializer(many=True, read_only=True)
+class StudySerializer(WritableNestedModelSerializer):
+    interventions = InterventionSerializer(many=True)
+    conditions = ConditionSerializer(many=True)
+    eligibilities = EligibilitySerializer(many=True)
 
     class Meta:
         model = Study
