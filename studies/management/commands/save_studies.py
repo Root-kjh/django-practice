dab305bb952eb8da7b97013a90a1d81af3029487
root-kjh <wldnro3141@gmail.com>
2023-02-27 21:39:19 +0900

    feat: add UPDATE_ORIGINAL_DATA command

--- studies/management/commands/save_studies.py
+++ studies/management/commands/save_studies.py
@@ -1,6 +1,6 @@
 from enum import Enum
 from django.core.management.base import BaseCommand
-from studies.batch_tasks import save_all_studies, convert_studies, translate_studies, save_study_original_datas, save_all_new_studies, save_new_study_original_datas
+from studies.batch_tasks import save_all_studies, convert_studies, translate_studies, save_study_original_datas, save_all_new_studies, save_new_study_original_datas, update_study_original_data
 
 class CommandAction(Enum):
     SAVE_ALL_STUDIES = "save_all_studies"
@@ -9,6 +9,7 @@ class CommandAction(Enum):
     TRANSLATE = "translate"
     SAVE_ALL_NEW_STUDIES = "save_all_new_studies"
     SAVE_NEW_ORIGINAL_DATA = "save_new_original_data"
+    UPDATE_ORIGINAL_DATA = "update_original_data"
 
 
 class Command(BaseCommand):
@@ -21,6 +22,7 @@ class Command(BaseCommand):
         CommandAction.TRANSLATE: translate_studies,
         CommandAction.SAVE_ALL_NEW_STUDIES: save_all_new_studies,
         CommandAction.SAVE_NEW_ORIGINAL_DATA: save_new_study_original_datas,
+        CommandAction.UPDATE_ORIGINAL_DATA: update_study_original_data,
     }
 
     def add_arguments(self, parser):
@@ -66,6 +68,14 @@ class Command(BaseCommand):
             const=CommandAction.SAVE_NEW_ORIGINAL_DATA,
             help="save new original data",
         )
+        parser.add_argument(
+            "--update-original-data",
+            action="store_const",
+            dest="action",
+            const=CommandAction.UPDATE_ORIGINAL_DATA,
+            help="update original data",
+        )
+
 
     def handle(self, *args, **options):
         self.actions[options["action"]]()
